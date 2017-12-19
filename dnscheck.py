0dcbff14490a5a1fb42765db8791c222a99611e3
mrbegley <34690968+mrbegley@users.noreply.github.com>
2017-12-19 12:04:56 -0800

    Add files via upload

---
+++ dnscheck.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+# a simple dns check
+
+import os
+import sys
+import socket
+
+the_IP = raw_input("\n[+]IP Address/Hostname: ")
+
+fqdn = socket.gethostbyaddr(the_IP)
+hostname1 = socket.gethostbyname(the_IP)
+name_ex = socket.gethostbyname_ex(the_IP)
+
+print "-" * 50
+print "\n[+] Checking DNS of %s...\n" % the_IP
+print "-" * 50
+
+print "[+]", fqdn
+print "[+]", hostname1
+print "[+]", name_ex
+print "-" * 50
+print "\n[+] Complete"
