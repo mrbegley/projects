4eee44a551dc9ca92af1588634f854500ed342e3
mrbegley <34690968+mrbegley@users.noreply.github.com>
2017-12-19 12:05:31 -0800

    Update dnscheck.py

--- dnscheck.py
+++ dnscheck.py
@@ -12,12 +12,12 @@ fqdn = socket.gethostbyaddr(the_IP)
 hostname1 = socket.gethostbyname(the_IP)
 name_ex = socket.gethostbyname_ex(the_IP)
 
-print "-" * 50
+print "-" * 30
 print "\n[+] Checking DNS of %s...\n" % the_IP
-print "-" * 50
+print "-" * 30
 
 print "[+]", fqdn
 print "[+]", hostname1
 print "[+]", name_ex
-print "-" * 50
+print "-" * 30
 print "\n[+] Complete"
