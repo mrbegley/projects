cf1807c5d64802058dc0d68eec8f07b43c9829fa
justin <justin@bigthings.xyz>
2018-01-10 22:56:49 +0000

    Changes to be committed: 	modified:   SSH.py

--- Pynet/SSH.py
+++ Pynet/SSH.py
@@ -7,4 +7,6 @@ except:
 def ssh_command(ip, user, passwd, command):
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-	
+
+
+# Coming soon. Very lazy
