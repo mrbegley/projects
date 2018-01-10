0bed5cba3c662af2eb826d4287e58ba8f213bf5b
justin <justin@bigthings.xyz>
2018-01-10 22:41:12 +0000

    Changes to be committed: 	new file:   Pynet/SSH.py 	new file:   Random_Projects/thenotepad.py 	new file:   Scan/dnscheck.py 	new file:   Scan/locate.py

---
+++ Pynet/SSH.py
@@ -0,0 +1,10 @@
+import threading
+try:
+    import paramiko
+except:
+    print "[+] Run 'pip install paramiko'"
+
+def ssh_command(ip, user, passwd, command):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+	
