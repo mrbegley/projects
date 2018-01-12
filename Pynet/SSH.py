7a531de17c34820d9feaf5f0778b7c68e53f8267
justin <justin@bigthings.xyz>
2018-01-12 19:59:29 +0000

    Changes to be committed: 	modified:   Pynet/SSH.py 	modified:   Scan/portscanner.py

--- Pynet/SSH.py
+++ Pynet/SSH.py
@@ -1,3 +1,5 @@
+import socket
+import sys
 import threading
 try:
     import paramiko
@@ -7,6 +9,12 @@ except:
 def ssh_command(ip, user, passwd, command):
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ip, username=user, password=passwd)
+    ssh_session = client.get_transport().open_session()
+    if ssh_session.active:
+        ssh_session.exec_command(command)
+        print ssh_session.recv(1024)
+    return
+ssh_command('192.168.1.12', 'justin', 'pythoniscool', 'id')
 
 
-# Coming soon. Very lazy
